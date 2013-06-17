0a02c57b322169f39e2d9cec863902612a5813bc
github username <github email address>
2013-06-17 15:58:51 -0400

    hi

--- lib/playlister.rb
+++ lib/playlister.rb
@@ -14,8 +14,34 @@ class Artist
     @@artists
   end
 
-  def intialize 
+  def initialize 
     @@artists << self
+    self.songs = []
   end
+
+  def songs_count
+    songs.size
+  end
+
+  def add_song (song)
+    songs << song
+
+  def genres
+   songs.collect {|song| song.genre}
+  end
+
+  end
+  
+  
+
 end
 
+
+class Song
+attr_accessor :genre
+end
+
+class Genre
+attr_accessor :name
+
+end
