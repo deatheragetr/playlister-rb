91b1eed63010c62f1332b3fc2de0b706b022a4af
deatheragetr <deatheragetr@vcu.edu>
2013-06-18 17:31:13 -0400

    stuff

--- lib/file_name_parser.rb
+++ lib/file_name_parser.rb
@@ -18,6 +18,40 @@ end
 
 def genre(songs_folder)
   genres = songs_folder.collect do |song_path|
-    song_path.split('[').last.split(']').strip
-  end  
+    song_path.split('[').last.split(']').first.strip
+  end 
+  genres.uniq 
 end
+
+################################################################
+# name is genre name
+Genre.new.name = name unless Genre.all.any? {|genre| genre.name == name } #avoid duplication
+## pass the following to song_instance.genre =
+Genre.all.select {|genre| genre.name == genre_name}
+
+##################################################################
+def data_builder
+  artist_name = song_path.split('/').last.split('-').first.strip
+  genre_name = ong_path.split('[').last.split(']').first.strip
+  song_name = song_path.split(' - ').last.split('[').first.strip
+  add_artist(artist_name)
+end  
+
+def add_artist(artist_name)
+  Artist.new.name = artist_name unless Artist.all.any? {|artist| artist.name == artist_name}
+end
+
+def add_genre(genre_name)
+  Genre.new.name = genre_name unless Genre.all.any? {|genre| genre.name == genre_name }
+end
+
+def add_song(song_name)
+end
+
+
+
+
+
+
+
+
