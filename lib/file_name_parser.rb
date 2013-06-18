5e1dd7433cf44f1da066ab9620812fccd9440c63
deatheragetr <deatheragetr@vcu.edu>
2013-06-18 18:01:58 -0400

    stufuffj

--- lib/file_name_parser.rb
+++ lib/file_name_parser.rb
@@ -30,11 +30,13 @@ Genre.new.name = name unless Genre.all.any? {|genre| genre.name == name } #avoid
 Genre.all.select {|genre| genre.name == genre_name}
 
 ##################################################################
-def data_builder
+def data_builder(song_path)
   artist_name = song_path.split('/').last.split('-').first.strip
-  genre_name = ong_path.split('[').last.split(']').first.strip
+  genre_name = song_path.split('[').last.split(']').first.strip
   song_name = song_path.split(' - ').last.split('[').first.strip
-  add_artist(artist_name)
+  artist = add_artist(artist_name)
+  genre = add_genre(genre_name)
+  add_song(song_name, genre, artist)
 end  
 
 def add_artist(artist_name)
@@ -45,10 +47,23 @@ def add_genre(genre_name)
   Genre.new.name = genre_name unless Genre.all.any? {|genre| genre.name == genre_name }
 end
 
-def add_song(song_name)
+def songs_artist(song, artist) 
+  song.artist = artist
 end
 
+def add_song(song_name, genre, artist)
+  song = Song.new
+  song.name = song_name
+  song.genre = genre
+  songs_artist(song, artist)
+  song 
+end
 
+def collect_data(songs_folder)
+  songs_folder.each do |record|
+    data_builder(record)
+  end
+end
 
 
 
